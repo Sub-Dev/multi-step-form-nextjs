9b2f538b165513f0d27c1ff3eda03721da4fb460
SubDev <bilomarin@gmail.com>
2024-10-25 16:50:15 -0300

    Refatorado o componente PersonalInfo: adicionado AddOnsForm, atualizado os componentes PersonalInfoForm e SelectPlan e ajuste o layout e o estilo e separado button next step e go back em outro componentes sendo NextButton.tsx e GoBackButton.tsx

--- src/components/PersonalInfo.tsx
+++ src/components/PersonalInfo.tsx
@@ -2,7 +2,8 @@
 import React, { useState } from 'react';
 import Sidebar from './Sidebar';
 import PersonalInfoForm from './PersonalInfoForm';
-import SelectPlan from './PersonalInfoPlan'; // Corrigi o nome da importação
+import SelectPlan from './PersonalInfoPlan'; // Corrigido o nome da importação para o componente de planos
+import AddOnsForm from './PersonalInfoAddOns'; // Importando o componente AddOnsForm
 
 const PersonalInfo = () => {
   const [formData, setFormData] = useState({
@@ -12,6 +13,8 @@ const PersonalInfo = () => {
   });
 
   const [currentStep, setCurrentStep] = useState(1); // Adiciona o estado para o passo atual
+  const [selectedAddOns, setSelectedAddOns] = useState<string[]>([]); // Adiciona o estado para add-ons
+  const [billingType, setBillingType] = useState<'monthly' | 'yearly'>('monthly'); // Adiciona o estado para o tipo de faturamento
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setFormData({
@@ -21,7 +24,7 @@ const PersonalInfo = () => {
   };
 
   return (
-    <div className="bg-white p-3 rounded-lg shadow-lg grid grid-cols-7 max-w-5xl pl-3 min-h-[500px]"> {/* Altura mínima ajustada */}
+    <div className="bg-white p-3 rounded-lg shadow-lg grid grid-cols-7 max-w-5xl pl-3 min-h-[550px] min-w-[75%]"> {/* Altura mínima ajustada */}
       <Sidebar currentStep={currentStep} />
       <div className="col-span-5 ml-3 flex flex-col justify-between min-h-[500px]"> {/* Flex e altura mínima garantida */}
         {currentStep === 1 && (
@@ -36,6 +39,16 @@ const PersonalInfo = () => {
           <SelectPlan
             currentStep={currentStep} // Passa o passo atual
             setCurrentStep={setCurrentStep} // Passa a função para mudar o passo
+            setBillingType={setBillingType} // Passa a função para definir o tipo de faturamento
+          />
+        )}
+        {currentStep === 3 && (
+          <AddOnsForm
+            selectedAddOns={selectedAddOns}       // Passa o estado selectedAddOns
+            setSelectedAddOns={setSelectedAddOns} // Passa a função setSelectedAddOns
+            currentStep={currentStep}
+            setCurrentStep={setCurrentStep}
+            billingType={billingType} // Passa o estado billingType
           />
         )}
       </div>
